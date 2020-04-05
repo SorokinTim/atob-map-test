f182579f015c97c6c3a7f63d8737e2738404f3ef
Tim Sorokin <timsorokin352@gmail.com>
2020-04-05 22:18:54 +0300

    Прототип регистрации запроса на доставку. Проинициализировал директорию, интегрировал mapbox (version 0.0.1)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+
+
+
+    componentDidMount() {
+        mapboxgl.accessToken = 'pk.eyJ1IjoidGltc29yb2tpbiIsImEiOiJjazhuM25ka3cwN3h6M2dwaWhvaHo3M2IxIn0.w-4ohye6olUU4eOGpeytaw';
+        let map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v10',
+            zoom: 10,
+            center: [27.559, 53.902]
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div id='map' style={{width: '30vw', height: '100vh', padding: 0, margin: 0}}/>
+                <div className="form-container"/>
+            </div>
+        )
+    }
 }
 
 export default App;
